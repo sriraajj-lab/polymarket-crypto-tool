4c8c336bafb17060937e2d3e7fa117baa34980a6
sriraajj-lab <sriraajj@gmail.com>
2026-03-12 21:12:27 +0530

    Fix __init__.py import error

--- polymarket_crypto_tool/__init__.py
+++ polymarket_crypto_tool/__init__.py
@@ -1,15 +1,7 @@
 """
-polymarket_crypto_tool - Monitor Polymarket crypto prediction markets for edges.
+polymarket_crypto_tool
 """
 
-from .main import run_bot, run_once
+from .config import Config
 from .analyzers import find_edges
-from .fetchers import fetch_polymarket_markets, fetch_crypto_prices, fetch_crypto_market_data
-from .config import config, Config
-
-__version__ = "0.1.0"
-__all__ = [
-    "run_bot", "run_once", "find_edges",
-    "fetch_polymarket_markets", "fetch_crypto_prices", "fetch_crypto_market_data",
-    "config", "Config",
-]
+from .fetchers import fetch_polymarket_markets, fetch_crypto_market_data, fetch_historical_data
